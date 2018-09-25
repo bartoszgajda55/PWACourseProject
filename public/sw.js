f24c970dc920fcd072ae069ef0e6a5fd08de5f54
bartoszgajda55 <bg@bartoszgajda.com>
2018-09-25 12:55:46 +0100

    Offline fallback page.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_STATIC_NAME = "static-v8";
+const CACHE_STATIC_NAME = "static-v9";
 const CACHE_DYNAMIC_NAME = "dynamic-v3";
 
 self.addEventListener("install", function (event) {
@@ -10,6 +10,7 @@ self.addEventListener("install", function (event) {
         cache.addAll([
           "/",
           "/index.html",
+          "/offline.html",
           "/src/js/app.js",
           "/src/js/feed.js",
           "/src/js/promise.js",
@@ -55,6 +56,12 @@ self.addEventListener("fetch", function (event) {
                 .then(function (cache) {
                   cache.put(event.request.url, res.clone());
                   return res;
+                })
+            })
+            .catch(function (err) {
+              return caches.open(CACHE_STATIC_NAME)
+                .then(function (cache) {
+                  return cache.match("/offline.html");
                 })
             });
         }
