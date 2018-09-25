fa0961015f8b16d67c01d04348e3f6c5cc319057
bartoszgajda55 <bg@bartoszgajda.com>
2018-09-25 14:15:18 +0100

    Strategy - network then cache strategy.

--- public/sw.js
+++ public/sw.js
@@ -70,6 +70,23 @@ self.addEventListener("activate", function (event) {
 //   );
 // });
 
+// Network then Cache Strategy
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function (res) {
+        return caches.open(CACHE_DYNAMIC_NAME)
+          .then(function (cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          })
+      })
+      .catch(function (err) {
+        return caches.match(event.request);
+      })
+  );
+});
+
 // Cache Only Strategy
 // self.addEventListener("fetch", function (event) {
 //   event.respondWith(
